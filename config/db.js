64a6cc2109aed789350b6ccacb19adc9d9e651a9
Sardor <sardorismoilov2005@gmail.com>
2025-06-22 22:32:20 +0900

    last commit

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    const db = await mongoose.connect(process.env.MONGO_URl);
+    const db = await mongoose.connect(process.env.MONGO_URL);
     console.info(`MongoDB successfully connected at ${db.connection.host}`);
     console.info(`Server is running on port:  http://localhost:${process.env.PORT}`)
 }
